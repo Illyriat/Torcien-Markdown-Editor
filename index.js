3a1fec801776d81b10497cfd871a9446a7c2bed6
Illyriat <109182598+Illyriat@users.noreply.github.com>
2025-03-29 00:50:57 +0000

    Image Assets Added

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const { app, BrowserWindow, Menu } = require('electron');
-const { autoUpdater } = require('electron-updater');
+const { app, BrowserWindow, Menu } = require("electron");
+const { autoUpdater } = require("electron-updater");
 
-const menu = require('./menu');
+const menu = require("./menu");
 
 let window;
 
-app.on('ready', () => {
-  window = new BrowserWindow({ width: 1400, height: 900, webPreferences: { nodeIntegration: true } });
-  window.loadFile('index.html');
+app.on("ready", () => {
+  window = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    webPreferences: { nodeIntegration: true },
+    icon: "./assets/Torcien-logo1.png",
+  });
+  window.loadFile("index.html");
   autoUpdater.checkForUpdatesAndNotify();
 });
 
